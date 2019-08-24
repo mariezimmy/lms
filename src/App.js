64dc4e3a87ffac878204432e7cce37482629e809
mariezimmy <mariezimmy@users.noreply.github.com>
2019-08-24 16:51:50 -0400

    added AWS Amplify beginning configurations

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
