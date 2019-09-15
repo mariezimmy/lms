e81feec4a7a2bd2130317a0abdffea684a90abe7
jcarn <jcarnahan3@gatech.edu>
2019-09-14 23:52:58 -0400

    initial set up of db

--- backEnd/app.py
+++ backEnd/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from waitress import serve
+from database import Database
 
 app = Flask(__name__)
 
@@ -10,4 +11,5 @@ def index():
 
 
 if __name__ == '__main__':
+    donne_database = Database(url = 'localhost', port = 27017, db_name = 'donne_documents')
     serve(app, host='0.0.0.0', port=3000)
--- backEnd/database.py
+++ backEnd/database.py
@@ -1,5 +1,29 @@
 import pymongo
 from pymongo import MongoClient
 
-client = MongoClient()
-client = MongoClient('localhost', 27017)
+
+class Database:
+
+    client = None
+    db = None
+    docs = None
+    db_up = False
+
+    def __init__(self, url = 'localhost', port = 27017, db_name = 'donne_documents'):
+        client = MongoClient(url, port)
+        self.db = client[db_name]
+        self.docs = self.db.documents
+        self.db_up = True
+
+    def get_doc(self, query):
+        doc = self.docs.find_one(query)
+        return doc
+
+    def get_all_docs(self):
+        #If we make this larger, we will remove the list() and just leave it as a cursor
+        return list(self.db.find())
+
+    def add_doc(self, new_doc):
+        docs = self.db.documents
+        new_doc_id = docs.insert_one(new_doc).inserted_id
+        return new_doc_id
