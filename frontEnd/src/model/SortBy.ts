30eb077fc0b2914037c9fee8e6c6b8578dfb2c21
Cinnamon18 <alice.ava.moh@gmail.com>
2019-10-02 13:31:18 -0400

    little refactor!

--- frontEnd/src/model/SortBy.ts
+++ frontEnd/src/model/SortBy.ts
@@ -1,8 +1,8 @@
 export default class SortBy {
-	constructor(public endpoint: string) {}
+	constructor(public endpoint: string, public description: string) {}
 
-	public static TitleAZ:SortBy = new SortBy("title/ascending")
-	public static TitleZA:SortBy = new SortBy("title/descending")
-	public static AuthorAZ:SortBy = new SortBy("author/ascending")
-	public static AuthorZA:SortBy = new SortBy("author/descending")
+	public static TitleAZ:SortBy = new SortBy("title/ascending", "Title (A - Z)")
+	public static TitleZA:SortBy = new SortBy("title/descending", "Title (Z - A)")
+	public static AuthorAZ:SortBy = new SortBy("author/ascending", "Author (A - Z)")
+	public static AuthorZA:SortBy = new SortBy("author/descending", "Author (Z - A)")
 }
